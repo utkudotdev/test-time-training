afa57463793f26e9c4efaa3f8275093fd4d81190
Utku Melemetci <55263178+utkudotdev@users.noreply.github.com>
2026-04-19 11:33:14 -0400

    Passive viewer

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 import mujoco
 import mujoco.viewer
+import time
 
 
 def main():
     with open("example.xml") as f:
         model = mujoco.MjModel.from_xml_string(f.read())
-    mujoco.viewer.launch(model)
-    print(model)
+    data = mujoco.MjData(model)
+
+    paused = False
+
+    def key_callback(keycode):
+        if chr(keycode) == " ":
+            nonlocal paused
+            paused = not paused
+
+    with mujoco.viewer.launch_passive(model, data, key_callback=key_callback) as viewer:
+        viewer.sync()
+
+        while viewer.is_running():
+            step_start = time.time()
+
+            if not paused:
+                mujoco.mj_step(model, data)
+            viewer.sync()
+
+            time_until_next_step = model.opt.timestep - (time.time() - step_start)
+            if time_until_next_step > 0:
+                time.sleep(time_until_next_step)
 
 
 if __name__ == "__main__":
